4050dd98a26ca4896733ece1c7009d1d789e6e6e
Dre9y <xxdregteilsxx@gmail.com>
2021-12-11 18:01:46 +0300

    Create Lab2.c

---
+++ Lab2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    float z1,z2,a;
+    scanf("%f",&a);
+     z1 = (sin(4*a)/(1+cos(4*a)))*(cos(2*a)/(1+cos(2*a))); 
+      z2 = 1/tan((3./2)*M_PI-a);  
+     printf("%f %f",z1,z2);
+    return 0;
+}

7e6fabb2bf1e622ada7dc555af44e71ab28d62a1
Dre9y <xxdregteilsxx@gmail.com>
2021-12-11 23:08:42 +0300

    TST1

--- Lab1.cpp
+++ Lab1.cpp
@@ -1,4 +1,22 @@
+#include "pch.h"
+#include "CppUnitTest.h"
 #include <stdio.h>
-int main() {
-   a+b; 
+#include <iostream>
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(Multiplicationtests)
+	{
+	public:
+		
+		TEST_METHOD(APLUSB)
+		{
+			int b=0,a=0;
+			int aplusb = b+B;
+			int actual =A+B;
+			Assert:: AreEqual(Aplusb, actual);
+		}
+
+	};
 }
